71c56037742a22a1fdd46f0977de4748c60ae2ee
Nik <nikstarall@gmail.com>
2023-10-25 12:45:11 +0700

    fix: некоторые поля jobs уставноленны в not null

--- src/models/jobs.py
+++ src/models/jobs.py
@@ -10,8 +10,8 @@ class Job(Base):
 
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True, comment="Идентификатор вакансии")
     user_id = sa.Column(sa.Integer, sa.ForeignKey('users.id'), comment="Идентификатор пользователя")
-    title = sa.Column(sa.String, comment="Название вакансии")
-    description = sa.Column(sa.String, comment="Описание ввкансии")
+    title = sa.Column(sa.String, comment="Название вакансии", nullable=False)
+    description = sa.Column(sa.String, comment="Описание ввкансии", nullable=False)
     salary_from = sa.Column(sa.DECIMAL, comment="Зарплата от")
     salary_to = sa.Column(sa.DECIMAL, comment="Зарплата до")
     is_active = sa.Column(sa.Boolean, default=False)
