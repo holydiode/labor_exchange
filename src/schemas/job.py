48372b27098224ab576db5222f84f52af175c7c4
Nik <nikstarall@gmail.com>
2023-10-26 10:26:03 +0700

    feat: добалвено описание и занчение по умолчанию для схем валидации

--- src/schemas/job.py
+++ src/schemas/job.py
@@ -1,25 +1,35 @@
 import datetime
 from typing import Optional
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
 class JobSchema(BaseModel):
-    id: Optional[int] = None
-    user_id: int
-    title: str
-    description: str
-    salary_from: Optional[int]
-    salary_to: Optional[int]
-    is_active: bool
-    created_at: datetime.datetime
+    """Схема вакансии"""
+    id: Optional[int] = Field(default=None, description="Идентификатор вакансии")
+    user_id: int = Field(description="Идентификатор работодателя разместившего вакансию")
+    title: str = Field(default="Разработчик", description="Название вакансии")
+    description: str = Field(default="", description="Описание вакансии")
+    salary_from: Optional[int] = Field(default=None, description="Минимальная заработная плата")
+    salary_to: Optional[int] = Field(default=None, description="Максимальная заработная плата")
+    is_active: bool = Field(default=True, description="Существует ли вакансия сейчас")
+    created_at: datetime.datetime = Field(default=datetime.datetime.utcnow(), description="Название вакансии")
 
     class Config:
         orm_mode = True
 
 
 class JobInputSchema(BaseModel):
+    """Схема используемая для создания новой вакансии для работодателя"""
     title: str = Field(default="Разработчик", description="Название вакансии")
     description: str = Field(default="", description="Описание вакансии")
     salary_from: Optional[int] = Field(default=None, description="Минимальная заработная плата")
     salary_to: Optional[int] = Field(default=None, description="Максимальная заработная плата")
     is_active: bool = Field(default=True, description="Существует ли вакансия сейчас")
+
+    @classmethod
+    @validator("salary_to")
+    def password_match(cls, v, values, **kwargs):
+        salary_from = values['salary_from']
+        if v is not None and salary_from is not None and v < values['salary_from']:
+            raise ValueError("Максимальная зарплата не может быть меньше минимальной")
+        return True
--- src/schemas/response.py
+++ src/schemas/response.py
@@ -1,13 +1,14 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class ResponseSchema(BaseModel):
-    id: Optional[int] = None
-    user_id: int
-    job_id: int
-    message: str
+    """Схема отклика работника на вакансию"""
+    id: Optional[int] = Field(default=None, description="Идентификатор отклика")
+    user_id: int = Field(description="Идентификатор работника, откликнувшегося на вакансию")
+    job_id: int = Field(description="Идентификатор вакансии")
+    message: str = Field(default="", description="Сопроводительное письмо")
 
     class Config:
         orm_mode = True
