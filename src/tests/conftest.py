867ec426eddadfe7782a0fb47d13f4faabd9cb72
Nik <nikstarall@gmail.com>
2023-10-31 02:04:56 +0700

    feat: Добавлена возможность тестировать БД без фиксации

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,15 +1,19 @@
 import asyncio
 
+from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
+from core import TokenGenerator
 from dependencies import get_db
 from fixtures.users import UserFactory
 from fastapi.testclient import TestClient
 from main import app
 import pytest_asyncio
 from unittest.mock import MagicMock
-from db_settings import SQLALCHEMY_DATABASE_URL, Base
+from db_settings import SQLALCHEMY_DATABASE_URL
+from schemas import UserInSchema
+from queries import user as user_queries
 
 
 @pytest_asyncio.fixture()
@@ -36,22 +40,53 @@ async def sa_session():
         await connection.close()
         await engine.dispose()
 
-real_db = pytest_asyncio.fixture(get_db)
 
-@pytest_asyncio.fixture(autouse=True, scope="function")
-async def client_app():
-    engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+@pytest_asyncio.fixture()
+async def client_app(sa_session: AsyncSession) -> AsyncClient:
+    app.dependency_overrides[get_db] = lambda: sa_session
 
-    client = TestClient(app)
-    try:
+    async with AsyncClient(app=app, base_url="http://test") as client:
         yield client
-    finally:
-        async with engine.begin() as conn:
-            await conn.run_sync(Base.metadata.drop_all)
-
 
 @pytest_asyncio.fixture(autouse=True)
 def setup_factories(sa_session: AsyncSession) -> None:
     UserFactory.session = sa_session
+
+
+@pytest_asyncio.fixture()
+async def user_access_token(sa_session) -> str:
+    user_json = {
+        "name": "user",
+        "email": "user@example.com",
+        "password": "123456789",
+        "password2": "123456789",
+        "is_company": False
+    }
+    await user_queries.create(sa_session, UserInSchema(**user_json))
+
+    return TokenGenerator.create_access_token({"sub": user_json["email"]}).token
+@pytest_asyncio.fixture()
+async def company_access_token(sa_session) -> str:
+    user_json = {
+        "name": "user",
+        "email": "company@example.com",
+        "password": "123456789",
+        "password2": "123456789",
+        "is_company": True
+    }
+    await user_queries.create(sa_session, UserInSchema(**user_json))
+
+    return TokenGenerator.create_access_token({"sub": user_json["email"]}).token
+@pytest_asyncio.fixture()
+async def user_refresh_token(sa_session) -> str:
+    user_json = {
+        "name": "user",
+        "email": "user@example.com",
+        "password": "123456789",
+        "password2": "123456789",
+        "is_company": False
+    }
+    await user_queries.create(sa_session, UserInSchema(**user_json))
+
+    return TokenGenerator.create_refresh_token({"sub": user_json["email"]}).token
+
