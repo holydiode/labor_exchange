715dd153e1621060b0243e6358875381c46b9d36
Nik <nikstarall@gmail.com>
2023-10-31 02:07:39 +0700

    chore: Убран избыточный код для получения пользователя по токену

--- src/dependencies/user.py
+++ src/dependencies/user.py
@@ -7,35 +7,22 @@ from models import User
 
 
 async def get_current_user(db: AsyncSession = Depends(get_db), token: str = Depends(JWTBearer())) -> User:
+    return await get_token_owner(db, token, "access")
+
+
+async def get_refreshed_user(db: AsyncSession = Depends(get_db), token: str = Depends(JWTBearer())) -> User:
+    return await get_token_owner(db, token, "refresh")
+
+
+async def get_token_owner(db: AsyncSession, token: str, waited_token_type: str) -> User:
     cred_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Credentials are not valid")
     payload = decode_token(token)
     if payload is None:
         raise cred_exception
     email: str = payload.get("sub")
     token_type: str = payload.get("token_type")
-    if token_type is None or token_type != "access":
+    if token_type is None or token_type != waited_token_type or email is None:
         raise cred_exception
-    if email is None:
-        raise cred_exception
-    user = await user_queries.get_by_email(db=db, email=email)
-    if user is None:
-        raise cred_exception
-    return user
-
-
-async def refresh_user(db: AsyncSession = Depends(get_db), token: str = Depends(JWTBearer())) -> User:
-    cred_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Credentials are not valid")
-    payload = decode_token(token)
-    if payload is None:
-        raise cred_exception
-    token_type: str = payload.get("token_type")
-    email: str = payload.get("sub")
-
-    if token_type is None or token_type != "refresh":
-        raise cred_exception
-    if email is None:
-        raise cred_exception
-
     user = await user_queries.get_by_email(db=db, email=email)
     if user is None:
         raise cred_exception
